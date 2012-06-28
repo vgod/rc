58fbe8bf94b8b2a378e78c4efb7d4e9d167ed216
Tsung-Hsiang Chang <vgod@vgod.tw>
2012-06-28 11:02:05 -0400

    install.sh rename existing rc files

--- install.sh
+++ install.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
 RC_HOME=`pwd`
 cd ..
-ln -sf $RC_HOME/zshrc .zshrc
-ln -sf $RC_HOME/zsh .zsh
-ln -sf $RC_HOME/screenrc .screenrc
+for TARGET in zshrc zsh screenrc;
+do
+   if [ -e ".$TARGET" ]; then
+      mv ".$TARGET" ".$TARGET.old"
+   fi
+   ln -s "$RC_HOME/$TARGET" ".$TARGET"
+done
 
